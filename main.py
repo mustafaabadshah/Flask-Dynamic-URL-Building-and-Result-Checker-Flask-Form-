3e82686a80d14582736824a71783b8fb2cb807df
Syed mustafa badshah <122392456+mustafaabadshah@users.noreply.github.com>
2024-08-13 00:47:14 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
-###  integrate html with flask
-### HTTP verb Get and Post
+### Integrate HTML With Flask
+### HTTP verb GET And POST
 
+##Jinja2 template engine
+'''
+{%...%} conditions,for loops
+{{    }} expressions to print output
+{#....#} this is for comments
+'''
+from flask import Flask,redirect,url_for,render_template,request
 
-from flask import Flask, redirect, url_for, render_template, request
+app=Flask(__name__)
 
-
-app = Flask(__name__)
-@app.route("/")
+@app.route('/')
 def welcome():
     return render_template('index.html')
 
@@ -14,39 +19,43 @@ def welcome():
 def success(score):
     res=""
     if score>=50:
-        res="Pass"
+        res="PASS"
     else:
-        res="Fail"
-    return render_template('/result.html',result=res)
+        res='FAIL'
+    exp={'score':score,'res':res}
+    return render_template('result.html',result=exp)
+
 
 @app.route('/fail/<int:score>')
 def fail(score):
-    return "The person has fail and marks is "+str(score)
+    return "The Person has failed and the marks is "+ str(score)
 
-@app.route("/fail/<int:score>")
-def fail(score):
-    return "The person has fail and marks is "+str(score)
-
-@app.route("/results/<int:marks>")
+### Result checker
+@app.route('/results/<int:marks>')
 def results(marks):
+    result=""
     if marks<50:
-        results="fail"
+        result='fail'
     else:
-        results="success"
-    return redirect(url_for(results, score=marks))
-   
-   #result checker submit html page
-@app.route("/submit", methods=["POST", "GET"])
+        result='success'
+    return redirect(url_for(result,score=marks))
+
+### Result checker submit html page
+@app.route('/submit',methods=['POST','GET'])
 def submit():
+    total_score=0
+    if request.method=='POST':
+        science=float(request.form['science'])
+        maths=float(request.form['maths'])
+        c=float(request.form['c'])
+        data_science=float(request.form['datascience'])
+        total_score=(science+maths+c+data_science)/4
+    res=""
+    return redirect(url_for('success',score=total_score))
 
-    if request.method == "POST":
-        science = float(request.form["science"])
-        maths = float(request.form["maths"])
-        c =float(request.form["c"])
-        datascience = float(request.form["datascience"])
-        total_score = ((science) + int(maths) + int(c) + int(datascience))/4
-        res=""    
-        return redirect(url_for('success', score=total_score))
+    
 
-if __name__ == "__main__":
+
+
+if __name__=='__main__':
     app.run(debug=True)
